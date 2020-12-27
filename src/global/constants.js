9a54ed3734d83148969b210b75da624e256433f6
Hang Thanh <hangthanh180599@gmail.com>
2020-12-27 21:55:10 +0700

    add colors pallete

---
+++ src/global/constants.js
@@ -0,0 +1,10 @@
+export const colors = {
+    pink1: '#FFF3F3',
+    pink2: '#FFE2E2',
+    pink3: '#FFC7C7',
+    pink4: '#E28F8F',
+    gray1: '#F6F6F6',
+    gray2: '#D6D6D6',
+    gray3: '#AAAAAA',
+    gray4: '#888993'
+}
