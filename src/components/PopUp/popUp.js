8ad6c730fb28a182c26e1b72842f05cc361355e1
1712397-KD <1712397@student.hcmus.edu.vn>
2020-12-29 02:11:32 +0700

    popup ukila

--- src/components/PopUp/popUp.js
+++ src/components/PopUp/popUp.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Dialog from '@material-ui/core/Dialog';
-import PersonIcon from '@material-ui/icons/Person';
-import AddIcon from '@material-ui/icons/Add';
-import Typography from '@material-ui/core/Typography';
-import { blue } from '@material-ui/core/colors';
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles, withStyles, fade } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import Dialog from "@material-ui/core/Dialog";
+import AddIcon from "@material-ui/icons/Add";
+import Typography from "@material-ui/core/Typography";
+import { blue } from "@material-ui/core/colors";
+import MuiDialogTitle from "@material-ui/core/DialogTitle";
+import MuiDialogContent from "@material-ui/core/DialogContent";
+import MuiDialogActions from "@material-ui/core/DialogActions";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import { customThemes } from "../../styles/theme";
+import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
+import { colors } from "../../global/constants";
+import { Box, Grid } from "@material-ui/core";
 
-const emails = ['Routine mùa hè', 'Routine mùa đông'];
+const emails = ["Routine mùa hè", "Routine mùa đông"];
 const useStyles = makeStyles({
   avatar: {
     backgroundColor: blue[100],
     color: blue[600],
   },
 });
+
+const styles = (theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: customThemes.BUTTON_HOVER,
+  },
+});
+
+const PinkOutlineButton = withStyles((theme) => ({
+  root: {
+    color: customThemes.BUTTON_HOVER,
+    "&:hover": {
+      backgroundColor: fade(customThemes.PRIMARY_PINK, 0.7),
+    },
+    textTransform: "capitalize",
+    width: "20%",
+    border: "1px solid",
+    borderColor: customThemes.BUTTON_HOVER,
+  },
+}))(Button);
+
+const DialogTitle = withStyles(styles)((props) => {
+  const { children, classes, onClose, ...other } = props;
+  return (
+    <MuiDialogTitle disableTypography className={classes.root} {...other}>
+      <Typography className={classes.title}>{children}</Typography>
+      {onClose ? (
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      ) : null}
+    </MuiDialogTitle>
+  );
+});
+
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
+
+const DialogActions = withStyles((theme) => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing(1),
+  },
+}))(MuiDialogActions);
 
 function SimpleDialog(props) {
   const classes = useStyles();
@@ -35,29 +104,59 @@ function SimpleDialog(props) {
   };
 
   return (
-    <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
-      <DialogTitle id="simple-dialog-title">Thêm vào Routine</DialogTitle>
-      <List>
-        {emails.map((email) => (
-          <ListItem button onClick={() => handleListItemClick(email)} key={email}>
+    <Dialog
+      fullWidth="true"
+      maxWidth="xs"
+      onClose={handleClose}
+      aria-labelledby="simple-dialog-title"
+      open={open}
+      scroll="paper"
+    >
+      <DialogTitle id="simple-dialog-title" onClose={handleClose}>
+        Thêm vào Routine
+      </DialogTitle>
+      <DialogContent dividers>
+        <List>
+          {emails.map((email) => (
+            <ListItem
+              button
+              onClick={() => handleListItemClick(email)}
+              key={email}
+            >
+              <ListItemAvatar>
+                <CheckCircleOutlineIcon
+                  style={{ color: colors.pink4, marginRight: 10 }}
+                />
+              </ListItemAvatar>
+              <Grid container direction="column">
+                <Grid item>
+                  <ListItemText primary={email} />
+                </Grid>
+                <Grid item>
+                  <Box fontStyle="italic" fontSize="small">Description</Box>
+                </Grid>
+              </Grid>
+            </ListItem>
+          ))}
+
+          <ListItem
+            autoFocus
+            button
+            onClick={() => handleListItemClick("addAccount")}
+          >
             <ListItemAvatar>
-              <Avatar className={classes.avatar}>
-                <PersonIcon />
+              <Avatar>
+                <AddIcon />
               </Avatar>
             </ListItemAvatar>
-            <ListItemText primary={email} />
+            <ListItemText primary="Tạo routine mới" />
           </ListItem>
-        ))}
+        </List>
+      </DialogContent>
 
-        <ListItem autoFocus button onClick={() => handleListItemClick('addAccount')}>
-          <ListItemAvatar>
-            <Avatar>
-              <AddIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Tạo routine mới" />
-        </ListItem>
-      </List>
+      <DialogActions>
+        <PinkOutlineButton onClick={handleClose}>Xác nhận</PinkOutlineButton>
+      </DialogActions>
     </Dialog>
   );
 }
@@ -88,9 +187,13 @@ function SimpleDialogDemo() {
       <Button variant="outlined" color="primary" onClick={handleClickOpen}>
         Open simple dialog
       </Button>
-      <SimpleDialog selectedValue={selectedValue} open={open} onClose={handleClose} />
+      <SimpleDialog
+        selectedValue={selectedValue}
+        open={open}
+        onClose={handleClose}
+      />
     </div>
   );
 }
 
-export {SimpleDialog, SimpleDialogDemo}
+export { SimpleDialog, SimpleDialogDemo };
